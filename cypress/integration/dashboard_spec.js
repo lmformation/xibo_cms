ed91ebf94f27444e180e7bc065f4b762c1b372e5
lmformation <59911659+lmformation@users.noreply.github.com>
2020-01-17 13:39:34 +0100

    Add files via upload

---
+++ cypress/integration/dashboard_spec.js
@@ -0,0 +1,47 @@
+describe('Dashboard', function() {
+
+    beforeEach(function() {
+        cy.login();
+    });
+
+    it('should be at the dashboard page', function() {
+
+        cy.visit('/statusdashboard');
+
+
+        cy.url().should('include', 'dashboard');
+
+        // Check for the dashboard elements
+        cy.contains('Bandwidth Usage');
+        cy.contains('Library Usage');
+        cy.contains('Display Activity');
+        cy.contains('Latest News');
+    });
+
+    it('should show the welcome tutorial, and then disable it', function() {
+        cy.server();
+        cy.route('POST', '/user/welcome').as('showTour');
+        cy.route('PUT', '/user/welcome').as('disableTour');
+
+        cy.visit('/statusdashboard');
+
+        // Open user dropdown menu
+        cy.get('.dropdown-toggle img.nav-avatar').click();
+
+        // Click Reshow welcome
+        cy.get('#reshowWelcomeMenuItem').click();
+
+        cy.wait('@showTour');
+
+        cy.get('.popover.tour').contains('Welcome to the Xibo CMS!');
+
+        // Click to disable welcome tour
+        cy.get('button[data-role="end"]').click();
+        cy.wait('@disableTour');
+
+        cy.visit('/statusdashboard').then(() => {
+            cy.wait(500);
+            cy.get('.popover.tour').should('not.be.visible');
+        });
+    });
+});
---
+++ cypress/integration/layout_designer_populated_spec.js
@@ -0,0 +1,389 @@
+describe.skip('Layout Designer (Populated)', function() { //FIXME: Tests skipped for now, need update to the new Layout Designer revamp
+
+    beforeEach(function() {
+        cy.login();
+
+        // Import existing
+        cy.importLayout('../assets/export_test_layout.zip').as('testLayoutId').then((res) => {
+
+            cy.checkoutLayout(res);
+
+            cy.goToLayoutAndLoadPrefs(res);
+        });
+    });
+
+    /* Disabled for testing speed reasons
+        afterEach(function() {
+            // Remove the created layout
+            cy.deleteLayout(this.testLayoutId);
+        });
+    */
+
+    // Open widget form, change the name and duration, save, and see the name change result
+    it('changes and saves widget properties', () => {
+        // Create and alias for reload widget
+        cy.server();
+        cy.route('/playlist/widget/form/edit/*').as('reloadWidget');
+
+        // Select the first widget from the first region on timeline ( image )
+        cy.get('#timeline-container [data-type="region"]:first-child [data-type="widget"]:first-child').click();
+
+        // Type the new name in the input
+        cy.get('#properties-panel input[name="name"]').clear().type('newName');
+
+        // Set a duration
+        cy.get('#properties-panel #useDuration').check();
+        cy.get('#properties-panel input[name="duration"]').clear().type(12);
+
+        // Save form
+        cy.get('#properties-panel button[data-action="save"]').click();
+
+        // Should show a notification for the name change
+        cy.get('.toast-success').contains('newName');
+
+        // Check if the values are the same entered after reload
+        cy.wait('@reloadWidget').then(() => {
+            cy.get('#properties-panel input[name="name"]').should('have.attr', 'value').and('equal', 'newName');
+            cy.get('#properties-panel input[name="duration"]').should('have.attr', 'value').and('equal', '12');
+        });
+    });
+
+    // Open region form, change the name, dimensions and duration, save, and see the name change result
+    it('changes and saves region properties', () => {
+
+        // Create and alias for reload region
+        cy.server();
+        cy.route('/region/form/edit/*').as('reloadRegion');
+
+        // Open navigator edit
+        cy.get('#layout-navigator #edit-btn').click();
+
+        // Select the first region on navigator
+        cy.get('#layout-navigator [data-type="region"]:first-child').click();
+
+        // Type the new name in the input
+        cy.get('#layout-navigator-properties-panel input[name="name"]').clear().type('newName');
+
+        // Save form
+        cy.get('#layout-navigator-navbar button#save-btn').click();
+
+        // Should show a notification for the name change
+        cy.get('.toast-success').contains('newName');
+
+        // Check if the values are the same entered after reload
+        cy.wait('@reloadRegion').then(() => {
+            // Select the first region on navigator
+            cy.get('#layout-navigator [data-type="region"]:first-child').click();
+
+            cy.get('#layout-navigator-properties-panel input[name="name"]').should('have.attr', 'value').and('equal', 'newName');
+        });
+    });
+
+    // On layout edit form, change background color and layer, save and check the changes
+    it('changes and saves layout properties', () => {
+
+        // Create and alias for reload layout
+        cy.server();
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+
+        // Change background color
+        cy.get('#properties-panel input[name="backgroundColor"]').clear().type('#ccc');
+
+        // Change layer
+        cy.get('#properties-panel input[name="backgroundzIndex"]').clear().type(1);
+
+        // Save form
+        cy.get('#properties-panel button[data-action="save"]').click();
+
+        // Should show a notification for the successful save
+        cy.get('.toast-success').contains('Edited');
+
+        // Check if the values are the same entered after reload
+        cy.wait('@reloadLayout').then(() => {
+            cy.get('#properties-panel input[name="backgroundColor"]').should('have.attr', 'value').and('equal', '#cccccc');
+            cy.get('#properties-panel input[name="backgroundzIndex"]').should('have.value', '1');
+        });
+    });
+
+    // On layout edit form, change background image check the changes
+    it('should change layout´s background image', () => {
+
+        // Create and alias for reload layout
+        cy.server();
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+
+        cy.get('#properties-panel #select2-backgroundImageId-container').click();
+
+        // Select the last image option available ( avoid result and message "options")
+        cy.get('.select2-container .select2-results .select2-results__option:not(.select2-results__option--load-more):not(.loading-results):not(.select2-results__message):first').click();
+
+        // Save form
+        cy.get('#properties-panel button[data-action="save"]').click();
+
+        cy.wait('@reloadLayout');
+
+        // Change background image
+        cy.get('#properties-panel #select2-backgroundImageId-container').should('have.attr', 'title').then((title) => {
+            cy.get('#properties-panel #select2-backgroundImageId-container').click();
+
+            // Select the last image option available ( avoid result and message "options")
+            cy.get('.select2-container .select2-results .select2-results__option:not(.select2-results__option--load-more):not(.loading-results):not(.select2-results__message):last').click();
+
+            // Save form
+            cy.get('#properties-panel button[data-action="save"]').click();
+
+            // Should show a notification for the successful save
+            cy.get('.toast-success').contains('Edited');
+
+            // Check if the values are the same entered after reload
+            cy.wait('@reloadLayout').then(() => {
+                cy.get('#properties-panel #select2-backgroundImageId-container').should('have.attr', 'title').and('not.include', title);
+            });
+        });
+    });
+
+    it('should add a audio clip to a widget, and adds a link to open the form in the timeline', () => {
+
+        cy.populateLibraryWithMedia();
+
+        // Create and alias for reload layout
+        cy.server();
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+
+        // Open toolbar Tools tab
+        cy.get('#layout-editor-toolbar .btn-menu-tab').contains('Widgets').should('be.visible').click();
+        cy.get('#layout-editor-toolbar .btn-menu-tab').contains('Tools').should('be.visible').click();
+
+        // Open the audio form
+        cy.dragToElement(
+            '#layout-editor-toolbar #content-0 .toolbar-pane-content [data-sub-type="audio"] .drag-area',
+            '#timeline-container [data-type="region"]:first-child [data-type="widget"]:nth-child(2)'
+        ).then(() => {
+
+            // Select the 1st option
+            cy.get('[data-test="widgetPropertiesForm"] #mediaId > option').eq(1).then(($el) => {
+                cy.get('[data-test="widgetPropertiesForm"] #mediaId').select($el.val());
+            });
+
+            // Save and close the form
+            cy.get('[data-test="widgetPropertiesForm"] [data-bb-handler="done"]').click();
+
+            // Check if the widget has the audio icon
+            cy.wait('@reloadLayout').then(() => {
+                cy.get('#timeline-container [data-type="region"]:first-child [data-type="widget"]:nth-child(2)')
+                    .find('i[data-property="Audio"]').click();
+
+                cy.get('[data-test="widgetPropertiesForm"]').contains('Audio for');
+            });
+        });
+    });
+
+    it('attaches expiry dates to a widget, and adds a link to open the form in the timeline', () => {
+
+        // Create and alias for reload layout
+        cy.server();
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+
+        // Open toolbar Tools tab
+        cy.get('#layout-editor-toolbar .btn-menu-tab').contains('Widgets').should('be.visible').click();
+        cy.get('#layout-editor-toolbar .btn-menu-tab').contains('Tools').should('be.visible').click();
+
+        // Open the audio form
+        cy.dragToElement(
+            '#layout-editor-toolbar #content-0 .toolbar-pane-content [data-sub-type="expiry"] .drag-area',
+            '#timeline-container [data-type="region"]:first-child [data-type="widget"]:nth-child(2)'
+        ).then(() => {
+
+            // Add dates
+            cy.get('[data-test="widgetPropertiesForm"] #fromDt_Link1').clear().type('2018-01-01');
+            cy.get('[data-test="widgetPropertiesForm"] #fromDt_Link2').clear().type('00:00');
+
+            cy.get('[data-test="widgetPropertiesForm"] #toDt_Link1').clear().type('2018-01-01');
+            cy.get('[data-test="widgetPropertiesForm"] #toDt_Link2').clear().type('23:45');
+
+            // Save and close the form
+            cy.get('[data-test="widgetPropertiesForm"] [data-bb-handler="done"]').click();
+
+            // Check if the widget has the audio icon
+            cy.wait('@reloadLayout').then(() => {
+                cy.get('#timeline-container [data-type="region"]:first-child [data-type="widget"]:nth-child(2)')
+                    .find('i[data-property="Expiry"]').click();
+
+                cy.get('[data-test="widgetPropertiesForm"]').contains('Expiry for');
+            });
+        });
+    });
+
+    // NOTE: Test skipped for now until transitions are enabled by default
+    it.skip('adds a transition to a widget, and adds a link to open the form in the timeline', () => {
+        // Create and alias for reload layout
+        cy.server();
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+
+        // Open toolbar Tools tab
+        cy.get('#layout-editor-toolbar .btn-menu-tab').contains('Tools').should('be.visible').click();
+
+        // Open the audio form
+        cy.dragToElement(
+            '#layout-editor-toolbar .toolbar-pane-content [data-sub-type="transitionIn"] .drag-area',
+            '#timeline-container [data-type="region"]:first-child [data-type="widget"]:nth-child(2)'
+        ).then(() => {
+
+            // Select the 1st option
+            cy.get('[data-test="widgetPropertiesForm"] #transitionType > option').eq(1).then(($el) => {
+                cy.get('[data-test="widgetPropertiesForm"] #transitionType').select($el.val());
+            });
+
+            // Save and close the form
+            cy.get('[data-test="widgetPropertiesForm"] [data-bb-handler="done"]').click();
+
+            // Check if the widget has the audio icon
+            cy.wait('@reloadLayout').then(() => {
+                cy.get('#timeline-container [data-type="region"]:first-child [data-type="widget"]:nth-child(2)')
+                    .find('i[data-property="Transition"]').click();
+
+                cy.get('[data-test="widgetPropertiesForm"]').contains('Edit in Transition for');
+            });
+        });
+    });
+
+    // Navigator
+    it('should change and save the region´s position', () => {
+
+        // Create and alias for position save and reload layout
+        cy.server();
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+        cy.route('/region/form/edit/*').as('reloadRegion');
+
+        cy.get('#layout-navigator [data-type="region"]').then(($originalRegion) => {
+            const regionId = $originalRegion.attr('id');
+
+            // Open navigator edit
+            cy.get('#layout-navigator #edit-btn').click();
+
+            // Select region
+            cy.get('#layout-navigator-content #' + regionId).click();
+
+            // Move region 50px for each dimension
+            cy.get('#layout-navigator-content #' + regionId).then(($movedRegion) => {
+
+                const regionOriginalPosition = {
+                    top: Math.round($movedRegion.position().top),
+                    left: Math.round($movedRegion.position().left)
+                };
+
+                const offsetToAdd = 50;
+
+                // Move the region
+                cy.get('#layout-navigator-content #' + regionId)
+                    .trigger('mousedown', {
+                        which: 1
+                    })
+                    .trigger('mousemove', {
+                        which: 1,
+                        pageX: $movedRegion.width() / 2 + $movedRegion.offset().left + offsetToAdd,
+                        pageY: $movedRegion.height() / 2 + $movedRegion.offset().top + offsetToAdd
+                    })
+                    .trigger('mouseup');
+
+                // Close the navigator edit
+                cy.wait('@reloadRegion');
+                cy.get('#layout-navigator #save-btn').click();
+
+                // Close navigator
+                cy.get('#layout-navigator #close-btn').click();
+
+                // Wait for the layout to reload
+                cy.wait('@reloadLayout');
+
+                // Open navigator edit
+                cy.get('#layout-navigator #edit-btn').click();
+
+                // Check if the region´s position are not the original
+                cy.get('#layout-navigator-content #' + regionId).then(($changedRegion) => {
+                    expect(Math.round($changedRegion.position().top)).to.not.eq(regionOriginalPosition.top);
+                    expect(Math.round($changedRegion.position().left)).to.not.eq(regionOriginalPosition.left);
+                });
+            });
+        });
+    });
+
+    it('should delete a widget using the toolbar bin', () => {
+        cy.server();
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+
+        // Select a widget from the navigator
+        cy.get('#layout-timeline [data-type="region"]:first-child [data-type="widget"]:first-child').click().then(($el) => {
+
+            const widgetId = $el.attr('id');
+
+            // Click trash container
+            cy.get('#layout-editor-toolbar a#trashContainer').click();
+
+            // Confirm delete on modal
+            cy.get('[data-test="deleteObjectModal"] button[data-bb-handler="confirm"]').click();
+
+            // Check toast message
+            cy.get('.toast-success').contains('Deleted');
+
+            // Wait for the layout to reload
+            cy.wait('@reloadLayout');
+
+            // Check that widget is not on timeline
+            cy.get('#layout-timeline [data-type="widget"]#' + widgetId).should('not.exist');
+        });
+    });
+
+    it('saves the widgets order when sorting by dragging', () => {
+        cy.server();
+        cy.route('POST', '**/playlist/order/*').as('saveOrder');
+        cy.route('/layout?layoutId=*').as('reloadLayout');
+
+        cy.get('#layout-timeline [data-type="region"]:first-child [data-type="widget"]:first-child').then(($oldWidget) => {
+
+            const offsetX = 50;
+
+            // Move to the second widget position ( plus offset )
+            cy.wrap($oldWidget)
+                .trigger('mousedown', {
+                    which: 1
+                })
+                .trigger('mousemove', {
+                    which: 1,
+                    pageX: $oldWidget.offset().left + $oldWidget.width() * 1.5 + offsetX
+                })
+                .trigger('mouseup', {force: true});
+
+            cy.wait('@saveOrder');
+
+            // Should show a notification for the order change
+            cy.get('.toast-success').contains('Order Changed');
+
+            // Reload layout and check if the new first widget has a different Id
+            cy.wait('@reloadLayout');
+
+            cy.get('#layout-timeline [data-type="region"]:first-child [data-type="widget"]:first-child').then(($newWidget) => {
+                expect($oldWidget.attr('id')).not.to.eq($newWidget.attr('id'));
+            });
+        });
+    });
+
+    it('should publish a layout and go into a published state', () => {
+
+        cy.server();
+        cy.route('PUT', '/layout/publish/*').as('layoutPublish');
+
+        cy.get('#layout-editor-toolbar a#publishLayout').click();
+
+        cy.get('button[data-bb-handler="Publish"]').click();
+
+        // Get the id from the published layout and check if the designer reloaded to the Read Only Mode of that layout
+        cy.wait('@layoutPublish').then((res) => {
+            // Check if the page redirected to the layout designer with the new published layout
+            cy.url().should('include', '/layout/designer/' + res.response.body.data.layoutId);
+
+            // Check if the read only message appears
+            cy.get('#read-only-message').should('exist');
+        });
+    });
+});
